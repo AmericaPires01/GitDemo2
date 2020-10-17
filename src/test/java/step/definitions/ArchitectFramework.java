7a175870b5970cac939d906dcb6d7d2c6bdaa16e
AmericaPires01 <americapires4@gmail.com>
2020-10-17 16:32:40 +0100

    Created ArchitectFramework class rebuilt and pushed to architect branch

---
+++ src/test/java/step/definitions/ArchitectFramework.java
@@ -0,0 +1,11 @@
+package step.definitions;
+
+public class ArchitectFramework {
+    // IntelliJ popped out a message asking to add this change to Git I opt to add manually.
+    public static void main(String[] args){
+        System.out.println("GitCucumber (project owner) in this scenario pretending to be an Architect created architect branch locally");
+        System.out.println("--- Then he created ArchitectFramework class (this class) and pushed the code to architect branch ---");
+        System.out.println("--- In this way Architect team can do their development without disturbing lets say the Tester team in master branch ---");
+    }
+
+}
