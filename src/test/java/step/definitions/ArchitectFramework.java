0067287bc544c9069d076b26b3330890aff286fe
AmericaPires01 <americapires4@gmail.com>
2020-10-17 17:41:58 +0100

    GitDemo2 Architect team member added to ArchitectFramework class

--- src/test/java/step/definitions/ArchitectFramework.java
+++ src/test/java/step/definitions/ArchitectFramework.java
@@ -6,6 +6,7 @@ public class ArchitectFramework {
         System.out.println("GitCucumber (project owner) in this scenario pretending to be an Architect created architect branch locally");
         System.out.println("--- Then he created ArchitectFramework class (this class) and pushed the code to architect branch ---");
         System.out.println("--- In this way Architect team can do their development without disturbing lets say the Tester team in master branch ---");
+        System.out.println("----- GitDemo2 Architect team member added to ArchitectFramework class ——" );
     }
 
 }
